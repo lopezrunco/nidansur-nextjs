2328bc01a48953482cdf542e1f0c1fee8654b8b6
Damián López <lopezrunco@gmail.com>
2022-07-25 21:18:11 +0530

    Services page & center TitleLight component

--- components/TitleLight.js
+++ components/TitleLight.js
@@ -5,7 +5,7 @@ const TitleLight = ({ titleLight, titleBold }) => {
         <div className={styles.title}>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-12'>
+                    <div className='col-12 d-flex flex-column justify-content-center align-items-center text-center'>
                         <h3>{titleLight} <b>{titleBold}</b></h3>
                         <div className={styles.separator}></div>
                     </div>
